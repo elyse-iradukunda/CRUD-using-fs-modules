0bde59a8b18ebc091b4cc59c74d2fd7a956f41ba
Your Name <ellycreativity8@gmail.com>
2026-06-30 13:54:47 +0200

    feat: adding read in fs modules

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 import express from "express";
-import fs from "fs"
+import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-const filePath = path.join("__dirname", "data.json")
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "data.json");
 
 if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath,"[]")
+  fs.writeFileSync(filePath, "[]");
 }
+
+const readData = () => {
+  const data = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(data);
+};
+
+console.log(readData())
